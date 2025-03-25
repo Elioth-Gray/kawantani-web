43ba4db5217856a601f6b74d7b16df105124e9fb
Elioth <dharsuthdra@gmail.com>
2025-03-26 01:45:01 +0800

    Adding Dashboard

---
+++ components/navbar/DashboardNavbar.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { User } from "@phosphor-icons/react/dist/ssr/User";
+import { CaretDown } from "@phosphor-icons/react/dist/ssr";
+import { Bell } from "@phosphor-icons/react/dist/ssr";
+
+const DashboardNavbar = () => {
+  const router = useRouter();
+
+  return (
+    <nav className="flex flex-row justify-between items-center text-black w-full bg-[#FCF7F1] py-[2.1rem] px-[9rem]">
+      <div className="flex flex-row justify-start items-center gap-[4rem]">
+        <Image
+          src="/images/logo2.png"
+          width={181}
+          height={58}
+          alt="logo"
+          className="cursor-pointer"
+          onClick={() => {
+            router.push("/");
+          }}
+        ></Image>
+        <ul className="flex flex-row justify-center items-center gap-[4.6rem] font-regular text-[1rem]">
+          <li className="hover:text-[#50B34B] transition-all duration-100 ease-in-out">
+            <Link href="/">Dashboard</Link>
+          </li>
+          <li className="hover:text-[#50B34B] transition-all duration-100 ease-in-out">
+            <Link href="/#about">Tentang</Link>
+          </li>
+          <li className="hover:text-[#50B34B] transition-all duration-100 ease-in-out">
+            <Link href="/plants">Tracking</Link>
+          </li>
+          <li className="hover:text-[#50B34B] transition-all duration-100 ease-in-out">
+            <Link href="/articles">Artikel</Link>
+          </li>
+          <li className="hover:text-[#50B34B] transition-all duration-100 ease-in-out">
+            <Link href="/workshops">Workshop</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-row justify-end items-center gap-[1.25rem]">
+        <div className="flex flex-row justify-center items-center gap-[0.3rem] cursor-pointer">
+          <div className="rounded-full border border-[#50B34B] p-[0.1rem] flex flex-col justify-center items-center">
+            <div className="p-[0.548rem] bg-white rounded-full flex flex-col justify-center items-center">
+              <User size={15} color="#fffffff" />
+            </div>
+          </div>
+          <CaretDown size={18} color="#fffff"></CaretDown>
+        </div>
+        <Bell size={21} color="#fffff"></Bell>
+      </div>
+    </nav>
+  );
+};
+
+export default DashboardNavbar;
---
+++ pages/Dashboard/DashboardMain.tsx
@@ -0,0 +1,335 @@
+import React from "react";
+import Image from "next/image";
+import { Drop } from "@phosphor-icons/react/dist/ssr";
+import { Check } from "@phosphor-icons/react/dist/ssr";
+import Link from "next/link";
+import ArticleCard from "@/components/cards/ArticleCard";
+import WorkshopCard from "@/components/cards/WorkshopCard";
+
+const DashboardMain = () => {
+  return (
+    <>
+      {/* Feature Section */}
+      <section className="px-[9rem] py-[6.3rem] bg-[#FCF7F1]">
+        {/* Weather Section */}
+        <div className="flex flex-col">
+          <h1 className="text-[#50B34B] text-[3rem] font-bold gap-[0.75rem]">
+            Pantau Cuaca
+          </h1>
+          <div className="flex flex-row justify-between">
+            <div className="flex flex-col justify-between items-start">
+              <h1 className="text-[1.5rem] font-semibold">11 Februari 2025</h1>
+              <h1 className="text-[1.5rem] font-semibold">
+                Gubeng, Surabaya, Jawa Timur
+              </h1>
+            </div>
+            <div className="flex flex-row justify-end items-center gap-[4rem]">
+              <div className="py-[3.3rem] px-[4rem] rounded-lg border border-black flex flex-col justify-center items-center w-[21.25rem]">
+                <div className="flex flex-row justify-center items-center gap-[1rem]">
+                  <Image
+                    src="/images/weather.svg"
+                    width={84}
+                    height={68}
+                    alt="logo"
+                    className="cursor-pointer"
+                  ></Image>
+                  <div className="flex flex-col justify-between items-start">
+                    <p className="text-[1rem] font-semibold">Kelembapan</p>
+                    <p className="text-[2.25rem] font-semibold">36°C</p>
+                  </div>
+                </div>
+              </div>
+              <div className="py-[3.3rem] px-[4rem] rounded-lg border border-black flex flex-col justify-center items-center w-[21.25rem]">
+                <div className="flex flex-row justify-center items-center gap-[1rem]">
+                  <Image
+                    src="/images/heat.svg"
+                    width={60}
+                    height={87}
+                    alt="logo"
+                    className="cursor-pointer"
+                  ></Image>
+                  <div className="flex flex-col justify-between items-start">
+                    <p className="text-[1rem] font-semibold">Suhu</p>
+                    <p className="text-[2.25rem] font-semibold">36°C</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Plants Section */}
+        <div className="flex flex-col mt-[6.3rem]">
+          <h1 className="text-[#50B34B] text-[3rem] font-bold gap-[0.75rem]">
+            Pantau Tanaman
+          </h1>
+          <div className="grid grid-cols-9 gap-x-[3.3rem]">
+            {/* Tracking Section */}
+            <div className="col-span-6 w-full py-[2rem] border border-black rounded-lg px-[3rem] flex flex-col justify-start items-start gap-[2rem]">
+              <div className="flex flex-row justify-center items-center gap-[1rem] h-fit">
+                <div className="w-[4.9rem] h-[4.9rem]">
+                  <Image
+                    src="/images/lemon.jpg"
+                    width={79}
+                    height={79}
+                    alt="logo"
+                    className="cursor-pointer object-cover h-full rounded-full"
+                  ></Image>
+                </div>
+                <div className="flex flex-col justify-start items-start">
+                  <p className="text-[1.6rem] font-semibold">Lemon Malang</p>
+                  <p className="text-[0.8rem">36 Hari menuju panen</p>
+                </div>
+              </div>
+              <div className="flex flex-col justify-start items-start gap-[0.75rem]">
+                <p className="text-[1.25rem] font-semibold">Hari</p>
+                <div className="flex flex-row justify-start items-center gap-[0.9rem]">
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    1
+                  </button>
+                  <button className="p-[0.8rem] bg-[#50B34B] border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold text-white">
+                    2
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    3
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    4
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    5
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    6
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    7
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    8
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    9
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    10
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    11
+                  </button>
+                  <button className="p-[0.8rem] bg-white border-[#CEDADE] rounded-full border-2 flex flex-col justify-center items-center w-[2rem] h-[2rem] cursor-pointer text-[1rem] font-semibold">
+                    12
+                  </button>
+                  <p className="text-[1rem] text-[#50B34B] font-semibold">
+                    Lihat Semuanya
+                  </p>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 w-full gap-x-[2.25rem]">
+                <div className="col-span-1 flex flex-col justify-start items-start w-full gap-[0.6rem]">
+                  <p className="text-[1.25rem] font-semibold">Tugas Harian</p>
+                  <div className="flex flex-col justify-start items-start w-full gap-[1.2rem]">
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                  </div>
+                </div>
+                <div className="col-span-1 flex flex-col justify-start items-start w-full gap-[0.6rem]">
+                  <p className="text-[1.25rem] font-semibold">
+                    Pengecekan Harian
+                  </p>
+                  <div className="flex flex-col justify-start items-start w-full gap-[1.2rem]">
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                    <button className="py-[0.8rem] px-[1rem] bg-[#50B34B] w-full rounded-lg border-[#CEDADE] border-2 flex flex-row justify-between items-center">
+                      <div className="flex flex-row justify-start items-center gap-[0.8rem]">
+                        <Drop size={21} color="#FFFFFF" weight="fill"></Drop>
+                        <p className="text-white font-medium text-[1rem]">
+                          Siram Tanaman
+                        </p>
+                      </div>
+                      <Check size={21} color="#FFFFFF" weight="fill"></Check>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* More Plants Section */}
+            <div className="col-span-3 bg-[#153236] rounded-lg py-[1.4rem] flex flex-col justify-start items-start px-[1.8rem] text-white gap-[2.1rem]">
+              <div className="w-full flex flex-row justify-between items-center">
+                <h1 className="text-[1.25rem] font-semibold">Tanamanmu</h1>
+                <p className="text-[#50B34B] text-[0.8rem]">Lihat semuanya</p>
+              </div>
+              <div className="flex flex-col justify-start items-start gap-[3rem]">
+                <div className="flex flex-row justify-center items-center gap-[1rem] h-fit">
+                  <div className="w-[3.1rem] h-[3.1rem]">
+                    <Image
+                      src="/images/lemon.jpg"
+                      width={79}
+                      height={79}
+                      alt="logo"
+                      className="cursor-pointer object-cover h-full rounded-full"
+                    ></Image>
+                  </div>
+                  <div className="flex flex-col justify-start items-start">
+                    <p className="text-[0.8rem] font-semibold">Lemon Malang</p>
+                    <p className="text-[0.75rem]">36 Hari menuju panen</p>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-[1rem] h-fit">
+                  <div className="w-[3.1rem] h-[3.1rem]">
+                    <Image
+                      src="/images/lemon.jpg"
+                      width={79}
+                      height={79}
+                      alt="logo"
+                      className="cursor-pointer object-cover h-full rounded-full"
+                    ></Image>
+                  </div>
+                  <div className="flex flex-col justify-start items-start">
+                    <p className="text-[0.8rem] font-semibold">Lemon Malang</p>
+                    <p className="text-[0.75rem]">36 Hari menuju panen</p>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-[1rem] h-fit">
+                  <div className="w-[3.1rem] h-[3.1rem]">
+                    <Image
+                      src="/images/lemon.jpg"
+                      width={79}
+                      height={79}
+                      alt="logo"
+                      className="cursor-pointer object-cover h-full rounded-full"
+                    ></Image>
+                  </div>
+                  <div className="flex flex-col justify-start items-start">
+                    <p className="text-[0.8rem] font-semibold">Lemon Malang</p>
+                    <p className="text-[0.75rem]">36 Hari menuju panen</p>
+                  </div>
+                </div>
+                <div className="flex flex-row justify-center items-center gap-[1rem] h-fit">
+                  <div className="w-[3.1rem] h-[3.1rem]">
+                    <Image
+                      src="/images/lemon.jpg"
+                      width={79}
+                      height={79}
+                      alt="logo"
+                      className="cursor-pointer object-cover h-full rounded-full"
+                    ></Image>
+                  </div>
+                  <div className="flex flex-col justify-start items-start">
+                    <p className="text-[0.8rem] font-semibold">Lemon Malang</p>
+                    <p className="text-[0.75rem]">36 Hari menuju panen</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="px-[9rem] py-[6.3rem] bg-white">
+        <div className="w-full flex flex-col justify-start items-start gap-[2.4rem]">
+          <div className="w-full flex flex-col justify-start items-start">
+            <div className="flex flex-row justify-between items-center w-full">
+              <h1 className="text-black text-[3rem] font-bold gap-[0.75rem]">
+                Baca Artikel
+              </h1>
+              <Link href="" className="text-[1.2rem] text-[#50B34B]">
+                Lihat semuanya
+              </Link>
+            </div>
+          </div>
+          <div className="w-full grid grid-cols-4 h-full gap-x-[2.25rem] gap-y-[2.25rem]">
+            <ArticleCard
+              imageURL="/images/bayam.jpg"
+              title="Teknik Agar Bayam Tidak Rusak Saat Masa Tanam"
+              date="14 Februari 2025"
+            ></ArticleCard>
+            <ArticleCard
+              imageURL="/images/bayam.jpg"
+              title="Teknik Agar Bayam Tidak Rusak Saat Masa Tanam"
+              date="14 Februari 2025"
+            ></ArticleCard>
+            <ArticleCard
+              imageURL="/images/bayam.jpg"
+              title="Teknik Agar Bayam Tidak Rusak Saat Masa Tanam"
+              date="14 Februari 2025"
+            ></ArticleCard>
+            <ArticleCard
+              imageURL="/images/bayam.jpg"
+              title="Teknik Agar Bayam Tidak Rusak Saat Masa Tanam"
+              date="14 Februari 2025"
+            ></ArticleCard>
+          </div>
+        </div>
+        <div className="w-full flex flex-col justify-start items-start gap-[2.4rem] mt-[6.3rem]">
+          <div className="w-full flex flex-col justify-start items-start">
+            <div className="flex flex-row justify-between items-center w-full">
+              <h1 className="text-black text-[3rem] font-bold gap-[0.75rem]">
+                Ikuti Workshop
+              </h1>
+              <Link href="" className="text-[1.2rem] text-[#50B34B]">
+                Lihat semuanya
+              </Link>
+            </div>
+          </div>
+          <div className="w-full grid grid-cols-2 h-full gap-x-[2.25rem] gap-y-[2.25rem]">
+            <WorkshopCard
+              imageURL="/images/workshop-image.jpg"
+              title="Teknik Genjot Padi Untuk Keberlanjutan Pangan Jawa Tengah"
+              date="Jumat, 15 Februari 2025"
+              location="Balai Kota Solo, Jawa Tengah"
+            ></WorkshopCard>
+            <WorkshopCard
+              imageURL="/images/workshop-image.jpg"
+              title="Teknik Genjot Padi Untuk Keberlanjutan Pangan Jawa Tengah"
+              date="Jumat, 15 Februari 2025"
+              location="Balai Kota Solo, Jawa Tengah"
+            ></WorkshopCard>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default DashboardMain;
