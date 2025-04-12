58ad7256c00a9cf0c44f49d3edaba3fa37922ec1
Elioth <dharsuthdra@gmail.com>
2025-04-12 16:40:21 +0700

    Workshop Registration

---
+++ app/workshops/[id]/details/registration/page.tsx
@@ -0,0 +1,8 @@
+import WorkshopRegistrationPage from "@/pages/WorkshopRegistration/WorkshopRegistrationPage";
+import React from "react";
+
+const page = () => {
+  return <WorkshopRegistrationPage></WorkshopRegistrationPage>;
+};
+
+export default page;
