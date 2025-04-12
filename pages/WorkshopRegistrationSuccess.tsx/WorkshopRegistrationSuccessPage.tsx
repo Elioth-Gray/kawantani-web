b9903c854621b0d39d13ba5ef64594995fdf07c8
Elioth <dharsuthdra@gmail.com>
2025-04-12 19:06:44 +0700

    Workshop Done

---
+++ pages/WorkshopRegistrationSuccess.tsx/WorkshopRegistrationSuccessPage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import WorkshopRegistrationSuccessHeader from "./WorkshopRegistrationSuccessHeader";
+import WorkshopRegistrationSuccessMain from "./WorkshopRegistrationSuccessMain";
+import Footer from "@/components/footer/Footer";
+
+const WorkshopRegistrationSuccessPage = () => {
+  return (
+    <>
+      <WorkshopRegistrationSuccessHeader></WorkshopRegistrationSuccessHeader>
+      <WorkshopRegistrationSuccessMain></WorkshopRegistrationSuccessMain>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default WorkshopRegistrationSuccessPage;
