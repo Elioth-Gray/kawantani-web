c45aa40cab2d4b3e1a4ea02c657b8a9b683d4789
Elioth <dharsuthdra@gmail.com>
2025-04-16 16:33:17 +0700

    Finalizing Style

--- app/facilitator/login/page.tsx
+++ app/facilitator/login/page.tsx
@@ -1,5 +1,6 @@
 import FacilitatorLoginPage from "@/pages/facilitator/Login/FacilitatorLoginPage";
 import React from "react";
+import "../dashboard/admin.css";
 
 const page = () => {
   return <FacilitatorLoginPage></FacilitatorLoginPage>;
